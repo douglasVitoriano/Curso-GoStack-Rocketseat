7025cfe0bc16cce9fd1afe6fd488a383bc275b55
Douglas <douglas@insti.com.br>
2020-06-23 01:32:49 -0300

    GoStack Front-end com ReactJS

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
+import Header from './components/Header';
+
+function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    
+    function handleAddProject() {        
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        console.log(projects);
+    }
+
+    return (
+        <>          
+            <Header title="Projects" />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>    
+    )
+    
+}
+
+export default App;
